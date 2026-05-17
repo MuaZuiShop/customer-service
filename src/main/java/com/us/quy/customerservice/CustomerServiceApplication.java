276c067f164ed40447b5df1eb7cbb2d221acee0a
DangQuy <dangquyy1108@gmail.com>
2026-05-17 21:15:05 +0700

    enhance: config auth

--- src/main/java/com/us/quy/customerservice/CustomerServiceApplication.java
+++ src/main/java/com/us/quy/customerservice/CustomerServiceApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+    "com.us.quy",
+    "com.quy.common.core",
+    "com.quy.common.servlet"
+})
 @EnableDiscoveryClient
 public class CustomerServiceApplication {
 
--- src/main/java/com/us/quy/customerservice/controller/HeathCheckController.java
+++ src/main/java/com/us/quy/customerservice/controller/HeathCheckController.java
@@ -1,10 +1,15 @@
 package com.us.quy.customerservice.controller;
 
+import com.quy.common.core.security.ERole;
+import com.quy.common.servlet.services.UserContext;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/health-check")
 @Tag(name = "Health Check", description = "API dùng để kiểm tra trạng thái hoạt động của hệ thống")
+@Slf4j
 public class HeathCheckController {
 
     @GetMapping
@@ -28,4 +34,14 @@ public class HeathCheckController {
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("Hello World!, I'm CUSTOMER SERVICE");
     }
+
+
+    @GetMapping("/health-check1")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<String> healthCheckWithToken() {
+        String userId = UserContext.getUserId();
+        List<ERole> roles = UserContext.getUserRoles();
+        return ResponseEntity.ok("userId: " + userId + ", roles: " + roles);
+    }
+
 }
